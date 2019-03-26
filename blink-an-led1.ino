0f7eeb658d0f7edd612d1429f912174641bc0d96
IsaacGleeson <48934469+IsaacGleeson@users.noreply.github.com>
2019-03-26 12:47:04 +1100

    Add files via upload

---
+++ blink-an-led1.ino
@@ -0,0 +1,100 @@
+
+
+int led1 = D7;
+
+
+void setup() 
+{
+
+  pinMode(led1, OUTPUT);
+
+}
+
+
+void loop() {
+  // I
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  // S
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  // A
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  // A
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  // C
+  
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  delay(500);
+  
+
+
+}
+
